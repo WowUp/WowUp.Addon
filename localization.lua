c0b60446d011d1107bc7d1daba60f75e1016889e
Lynn <kjarli@gmail.com>
2020-11-07 20:52:39 +0100

    message -> chat message

--- localization.lua
+++ localization.lua
@@ -3,7 +3,7 @@ WowUpTexts = {
     ['updateNotification'] = '%s Notification: You have %d addon(s) to be updated',
     ['noUpdatesNotification'] = '%s Notification: all addons are up-to-date',
     ['showPopupNotificationCheckboxText'] = 'Show a popup with a notification after loading when updates are available',
-    ['showChatNotificationCheckboxText'] = 'Show a message indicating whether or not updates are available',
+    ['showChatNotificationCheckboxText'] = 'Show a chat message indicating whether or not updates are available',
 }
 local locale = GetLocale()
 
