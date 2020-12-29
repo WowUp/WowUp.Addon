bc3fd7e4a5813994519c41d365e817e253bb62cb
Nils89 <nils-reinecke@hotmail.de>
2020-12-29 19:34:09 +0100

    PR: Make addon local

---
+++ locals/deDE.lua
@@ -0,0 +1,9 @@
+-- deDE localization
+local L = LibStub("AceLocale-3.0"):NewLocale("WOWUP", "deDE")
+
+L["Notification"] = "Benachrichtigung"
+L["You have %d addon to be updated"] = "Du kannst %d Addon aktualisieren"
+L["You have %d addons to be updated"] = "Du kannst %d Addons aktualisieren"
+L["All addons are up-to-date"] = "Alle Addons sind aktuell"
+L["Show a popup with a notification after loading when updates are available"] = "Zeige eine Popup-Benachrichtigung, wenn eine Addon Update verfügbar ist (nach dem Login)"
+L["Show a chat message indicating whether or not updates are available"] = "Zeige eine Chatnachricht, ob ein Addon Update verfügbar ist oder ob alle Addons aktuell sind (nach dem Login)"
