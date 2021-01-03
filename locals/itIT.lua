2ad65eacc91a1aab07e75c12374aff6bb6bbbb3f
Bito <72380594+Bito98@users.noreply.github.com>
2021-01-03 16:21:11 +0100

    Update itIT.lua

--- locals/itIT.lua
+++ locals/itIT.lua
@@ -1,9 +1,9 @@
 -- itIT localization
 local L = LibStub("AceLocale-3.0"):NewLocale("WOWUP", "itIT")
 
-L["Notification"] = Notifica
-L["You have %d addon to be updated"] = Hai %d addon da aggiornare
-L["You have %d addons to be updated"] = Hai %d addons da aggiornare
-L["All addons are up-to-date"] = Tutti gli addons sono aggiornati
-L["Show a popup with a notification after loading when updates are available"] = Mostra un popup di notifica quando sono disponibili degli aggiornamenti (dopo il Login)
-L["Show a chat message indicating whether or not updates are available"] = Mostra un messaggio in chat se sono disponibili o meno degli aggiornamenti (dopo il Login)
+L["Notification"] = "Notifica"
+L["You have %d addon to be updated"] = "Hai %d addon da aggiornare"
+L["You have %d addons to be updated"] = "Hai %d addons da aggiornare"
+L["All addons are up-to-date"] = "Tutti gli addons sono aggiornati"
+L["Show a popup with a notification after loading when updates are available"] = "Mostra un popup di notifica quando sono disponibili degli aggiornamenti (dopo il Login)"
+L["Show a chat message indicating whether or not updates are available"] = "Mostra un messaggio in chat se sono disponibili o meno degli aggiornamenti (dopo il Login)"
