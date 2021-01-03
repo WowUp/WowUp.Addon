527075c7dcfd4e4f9bc4ffa6159c6d749219a523
dadec666 <dadec666@gmail.com>
2021-01-03 17:05:08 +0300

    Update ruRU.lua

--- locals/ruRU.lua
+++ locals/ruRU.lua
@@ -2,9 +2,9 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("WOWUP", "ruRU")
 if not L then return end
 
-L["Notification"] = true
-L["You have %d addonto be updated"] = true
-L["You have %d addons to be updated"] = true
-L["All addons are up-to-date"] = true
-L["Show a popup with a notification after loading when updates are available"] = true
-L["Show a chat message indicating whether or not updates are available"] = true
+L["Notification"] = "Уведомление"
+L["You have %d addonto be updated"] = "Вам нужно обновить %d аддонов"
+L["You have %d addons to be updated"] = "У вас есть %d аддонов для обновления"
+L["All addons are up-to-date"] = "Все дополнения обновлены"
+L["Show a popup with a notification after loading when updates are available"] = "Показывать всплывающее окно с уведомлением после загрузки, когда доступны обновления"
+L["Show a chat message indicating whether or not updates are available"] = "Показать сообщение чата, указывающее, доступны ли обновления"
