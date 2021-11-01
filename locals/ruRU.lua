eda45d47eb53d106cc1c3d7e76c598d43080bba5
Oleksandr <72464426+Medoke@users.noreply.github.com>
2021-11-01 17:59:44 +0200

    Update ruRU.lua

--- locals/ruRU.lua
+++ locals/ruRU.lua
@@ -4,8 +4,8 @@ if not L then return end
 
 L["Notification"] = "Уведомление"
 L["You have %d addon to be updated"] = "Вам нужно обновить %d"
-L["You have %d addons to be updated"] = "У вас есть %d аддонов для обновления"
-L["All addons are up-to-date"] = "Все дополнения обновлены"
+L["You have %d addons to be updated"] = "У вас есть %d модификаций  для обновления"
+L["All addons are up-to-date"] = "Все модификации обновлены"
 L["Show a popup with a notification after loading when updates are available"] = "Показывать всплывающее окно с уведомлением после загрузки, когда доступны обновления"
 L["Show a chat message indicating whether or not updates are available"] = "Показать сообщение чата, указывающее, доступны ли обновления"
 L["Click to open settings"] = true
